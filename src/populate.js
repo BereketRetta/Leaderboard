3985d328339c51748a205cd24b867c5e1c26b306
Bereket Retta <bereket@etmsoftwareplc.com>
2022-02-01 20:05:08 +0300

    add js file

--- src/populate.js
+++ src/populate.js
@@ -1,15 +1,23 @@
-const index = () => {
-    const scores = document.querySelector('.scores');
-  
-    scores.innerHTML = `
-    <ul>
-    <li>Name:100</li>
-    <li class="li-grey">Name:10</li>
-    <li>Name:200</li>
-    <li class="li-grey">Name:40</li>
-    <li>Name:60</li>
-    <li class="li-grey">Name:60</li>
-    </ul>`;
-  };
-  
-  export default index;
+import axios from 'axios';
+
+const createGame = async () => {
+  const response = await axios.post('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/', { name: 'my cool game' });
+  const myArray = response.data.result.split(':');
+  const id = myArray[1].split(' ')[1];
+  return id;
+};
+
+const postInfo = async (id, name, val) => {
+  await axios.post(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${id}/scores`, {
+    user: name,
+    score: val,
+  });
+};
+
+const getData = async (id) => {
+  const res = await axios.get(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${id}/scores`);
+
+  return res.data.result;
+};
+
+export default { createGame, postInfo, getData };
